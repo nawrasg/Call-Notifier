b5cbf77b1a650fc46a0182cfb6cf15f8befcc749
Nawras GEORGI <nawrasg@gmail.com>
2015-12-08 20:12:11 +0100

    Add Atlantis option

--- app/src/main/java/fr/nawrasg/callnotifier/fragments/PrefFragment.java
+++ app/src/main/java/fr/nawrasg/callnotifier/fragments/PrefFragment.java
@@ -2,15 +2,19 @@ package fr.nawrasg.callnotifier.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+
 import fr.nawrasg.callnotifier.AboutActivity;
 import fr.nawrasg.callnotifier.R;
 
 public class PrefFragment extends PreferenceFragment{
 	private Preference mAbout;
+	private CheckBoxPreference mAtlantis;
 	private Context mContext;
 
 	@Override
@@ -27,5 +31,25 @@ public class PrefFragment extends PreferenceFragment{
 				return true;
 			}
 		});
+		mAtlantis = (CheckBoxPreference) findPreference("atlantis");
+		if(!checkAtlantis()){
+			mAtlantis.setEnabled(false);
+		}
+		mAtlantis.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				return false;
+			}
+		});
+	}
+
+	private boolean checkAtlantis(){
+		PackageManager nPM = mContext.getPackageManager();
+		try {
+			nPM.getPackageInfo("fr.nawrasg.atlantis", PackageManager.GET_ACTIVITIES);
+			return true;
+		} catch (PackageManager.NameNotFoundException e) {
+			return false;
+		}
 	}
 }
